bb2be7efcba35e7b0dc97c7a4cbb7049d4a06e41
sela891 <laetitia.sengseis@gmail.com>
2026-08-31 22:42:56 +0200

    Try h2

--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -53,9 +53,9 @@ export default async function ProductPage(props) {
 
         <div className={styles.productInfo}>
           <p className={styles.category}>{singleProduct.cat}</p>
-          <h1 data-test-id="product-title" className={styles.productTitle}>
+          <h2 data-test-id="product-title" className={styles.productTitle}>
             {singleProduct.name}
-          </h1>
+          </h2>
           <p data-test-id="product-price" className={styles.price}>
             €{singleProduct.price.toFixed(2)}
           </p>
