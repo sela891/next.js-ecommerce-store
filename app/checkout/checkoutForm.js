34604bc7e08a34a0849054d130afce350d13ce88
sela891 <laetitia.sengseis@gmail.com>
2026-09-19 20:10:06 +0200

    Remove Credit Card Holder Input

--- app/checkout/checkoutForm.js
+++ app/checkout/checkoutForm.js
@@ -76,14 +76,14 @@ export default function CheckoutForm() {
       <section className={styles.formSection}>
         <h2>Payment Details</h2>
 
-        <div className={styles.formGroup}>
+        {/*} <div className={styles.formGroup}>
           <label htmlFor="creditCardHolder">Credit Card Holder</label>
           <input
             id="creditCardHolder"
             data-test-id="checkout-credit-card-holder"
             required
           />
-        </div>
+        </div>*/}
 
         <div className={styles.formGroup}>
           <label htmlFor="creditCard">Credit Card Number</label>
